6a36a7e61c23b2c187990760151b6e89d73222fd
Andrew Tomaka <atomaka@gmail.com>
2015-10-06 11:30:39 -0400

    Switch to bootstrap for now

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem 'jquery-rails'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
-gem 'materialize-sass'
+gem 'bootstrap-sass'
 gem 'slim-rails'
 gem 'simple_form'
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,40 @@
 module ApplicationHelper
-  def icon(icon)
-    content_tag(:i, icon.to_s, class: 'material-icons')
+  def nav_link_to(text, path)
+    options = current_controller?(path) ? { class: 'active' } : {}
+
+    content_tag(:li, options) { link_to text, path }
+  end
+
+  def bootstrap_class_for(flash_type)
+    bootstrap_classes[flash_type] || flash_type.to_s
+  end
+
+  def glyph(icon, options = {})
+    options = options.reverse_merge(default_glyph_options)
+
+    classes = options[:color] + " " + options[:classes]
+    content_tag(:i, class: "glyphicon glyphicon-#{icon} " + classes) {}
+  end
+
+  private
+
+  def bootstrap_classes
+    {
+      'alert'   => 'alert-warning',
+      'error'   => 'alert-danger',
+      'notice'  => 'alert-info',
+      'success' => 'alert-success'
+    }
+  end
+
+  def default_glyph_options
+    {
+      :color   => '',
+      :classes => ''
+    }
+  end
+
+  def current_controller?(path)
+    path.split('/')[1] == params[:controller]
   end
 end
