caf71781890daba835eb2dd7f279bfcb48261da2
Andrew Tomaka <atomaka@gmail.com>
2015-10-14 11:19:48 -0400

    Fix undo link

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -5,7 +5,7 @@ class Admin::BaseController < ApplicationController
 
   def undo_link(item)
     view_context.link_to('Undo this change',
-                         admin_revert_version_path(id: item.version.last),
+                         admin_revert_version_path(id: item.versions.last),
                          method: :post)
   end
 end
