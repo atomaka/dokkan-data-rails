7a839563f145d13fcdb7909988c21f0b8594a1c0
Andrew Tomaka <atomaka@gmail.com>
2015-10-20 18:57:20 -0400

    This changed...whatever

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151020220202) do
+ActiveRecord::Schema.define(version: 20151020212132) do
 
   create_table "awaken_types", force: :cascade do |t|
     t.string   "name"
