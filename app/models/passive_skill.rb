23796373f081c9060c875ba42b02d82efe9f127e
Andrew Tomaka <atomaka@gmail.com>
2015-10-23 11:27:15 -0400

    Uniqueness constraint on passive skill

--- app/models/passive_skill.rb
+++ app/models/passive_skill.rb
@@ -2,7 +2,8 @@ class PassiveSkill < ActiveRecord::Base
   has_paper_trail
 
   validates :name, presence: true
-  validates :description, presence: true
+  validates :description, presence: true,
+                          uniqueness: { scope: :name }
 
   default_scope { order(:name) }
 end
