a0be5a27d2f266eb3f96a7b5a63e25f7bf38b2c1
Andrew Tomaka <atomaka@gmail.com>
2015-11-03 15:37:21 -0500

    Fix migration so it doesn't break things!

--- db/migrate/20151103193404_add_links_counter_cache_to_cards.rb
+++ db/migrate/20151103193404_add_links_counter_cache_to_cards.rb
@@ -2,6 +2,7 @@ class AddLinksCounterCacheToCards < ActiveRecord::Migration
   def up
     add_column :cards, :links_count, :integer, default: 0, null: false
 
+    PaperTrail.whodunnit = 1
     Card.reset_column_information
     Card.find_each do |card|
       card.update_attribute(:links_count, card.links.count)
