ab8b94468231a0790b042a42e5e8c95e799fca1f
Andrew Tomaka <atomaka@gmail.com>
2015-11-03 14:36:44 -0500

    Add counter cache for links

---
+++ db/migrate/20151103193404_add_links_counter_cache_to_cards.rb
@@ -0,0 +1,14 @@
+class AddLinksCounterCacheToCards < ActiveRecord::Migration
+  def up
+    add_column :cards, :links_count, :integer, default: 0, null: false
+
+    Card.reset_column_information
+    Card.find_each do |card|
+      card.update_attribute(:links_count, card.links.count)
+    end
+  end
+
+  def down
+    remove_column :cards, :links_count
+  end
+end
