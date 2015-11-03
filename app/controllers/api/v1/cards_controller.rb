1302454e5e60c94aa74124921c807d01e279bc39
Andrew Tomaka <atomaka@gmail.com>
2015-11-03 13:24:10 -0500

    Remove caching

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -1,12 +1,10 @@
 class Api::V1::CardsController < Api::V1::BaseController
   before_action :set_card, only: [:show]
   def index
-    @cards = Rails.cache.fetch('api_all_cards', expires_in: 2.minutes) do
-      Card.includes(:character, :rarity, :type, :leader_skill,
-                    :passive_skill, :super_attack, :awaken_type,
-                    :dokkan_card, :hp_stat, :atk_stat, :def_stat,
-                    :links)
-    end
+    @cards = Card.includes(:character, :rarity, :type, :leader_skill,
+                           :passive_skill, :super_attack, :awaken_type,
+                           :dokkan_card, :hp_stat, :atk_stat, :def_stat,
+                           :links)
 
     render json: @cards, root: false
   end
