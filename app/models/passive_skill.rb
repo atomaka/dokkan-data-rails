cd7e8f736f726243daf96cc7c3a67079fc72c142
Andrew Tomaka <atomaka@gmail.com>
2015-11-03 15:29:10 -0500

    Allow unnamed passive skills

--- app/models/passive_skill.rb
+++ app/models/passive_skill.rb
@@ -1,13 +1,16 @@
 class PassiveSkill < ActiveRecord::Base
   has_paper_trail
 
-  validates :name, presence: true
   validates :description, presence: true,
                           uniqueness: { scope: :name }
 
   default_scope { order(:name) }
 
   def full
-    "#{name} - #{description}"
+    "#{display_name} - #{description}"
+  end
+
+  def display_name
+    name.present? ? name : 'UNNAMED'
   end
 end
